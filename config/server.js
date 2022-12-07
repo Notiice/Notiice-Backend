fbf5ca03256b8116ed36fc934a74fd50528bfb23
Schmid Maximilian, GHR-OSA-NEX-31 <Maxi03milian@gmail.com>
2022-12-07 18:26:26 +0100

    added ngrok url

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://2af1-83-76-236-236.ngrok.io',
   app: {
     keys: env.array('APP_KEYS'),
   },
